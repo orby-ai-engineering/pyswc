15522d6773ff144830d0c46312f917ed3844d569
Chunliang Lyu <hi@chunlianglyu.com>
2023-10-05 11:09:32 -0700

    Add missing swc_common::GLOBALS

--- src/lib.rs
+++ src/lib.rs
@@ -8,12 +8,13 @@ use swc_common::{
     FilePathMapping,
     FileName,
     Mark,
+    GLOBALS,
     comments::{Comments},
 };
 use swc_ecma_transforms_base::resolver;
 use swc_error_reporters::handler::{
     HandlerOpts,
-    try_with_handler
+    try_with_handler,
 };
 use swc_ecma_visit::VisitMutWith;
 
@@ -26,41 +27,44 @@ fn parse(code: String) -> PyResult<String> {
     let filename = FileName::Anon;
     let skip_filename = true;
 
-    let program = try_with_handler(
-        c.cm.clone(),
-        HandlerOpts {
-            skip_filename,
-            ..Default::default()
-        },
-        |handler| {
-            c.run(|| {
-                let fm = c.cm.new_source_file(filename, code);
+    let program =
+        GLOBALS.set(&Default::default(), || {
+            try_with_handler(
+                c.cm.clone(),
+                HandlerOpts {
+                    skip_filename,
+                    ..Default::default()
+                },
+                |handler| {
+                    c.run(|| {
+                        let fm = c.cm.new_source_file(filename, code);
 
-                let comments = if options.comments {
-                    Some(c.comments() as &dyn Comments)
-                } else {
-                    None
-                };
+                        let comments = if options.comments {
+                            Some(c.comments() as &dyn Comments)
+                        } else {
+                            None
+                        };
 
-                let mut p = c.parse_js(
-                    fm,
-                    handler,
-                    options.target,
-                    options.syntax,
-                    options.is_module,
-                    comments,
-                )?;
+                        let mut p = c.parse_js(
+                            fm,
+                            handler,
+                            options.target,
+                            options.syntax,
+                            options.is_module,
+                            comments,
+                        )?;
 
-                p.visit_mut_with(&mut resolver(
-                    Mark::new(),
-                    Mark::new(),
-                    options.syntax.typescript(),
-                ));
+                        p.visit_mut_with(&mut resolver(
+                            Mark::new(),
+                            Mark::new(),
+                            options.syntax.typescript(),
+                        ));
 
-                Ok(p)
-            })
-        }
-    );
+                        Ok(p)
+                    })
+                },
+            )
+        });
 
     if program.is_err() {
         Err(PyValueError::new_err("Unable to parse the code"))
@@ -74,7 +78,7 @@ fn parse(code: String) -> PyResult<String> {
     }
 }
 
-/// A Python module implemented in Rust.
+/// Python module for SWC.
 #[pymodule]
 fn pyswc(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(parse, m)?)?;
